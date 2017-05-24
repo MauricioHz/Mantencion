74058f0b2fd10f5c41e2af83aef4e5d89e092c69
MauricioHz <mauriciohz2002@gmail.com>
2017-05-24 16:55:21 -0400

    nuevo campo actividad
    nuevo campo para la tabla manteción programa semanal.

--- Tables.sql
+++ Tables.sql
@@ -1,1 +1,17 @@
 /* Tablas modulo mantencion */
+
+--
+-- Estructura de la tabla `mantencion_programa_semana`
+--
+DROP TABLE IF EXISTS mantencion_programa_semana;
+CREATE TABLE mantencion_programa_semana (
+  id_semana int(2) NOT NULL AUTO_INCREMENT,
+  id_equipo int(3) NOT NULL,
+  id_plan int(3) NOT NULL,
+  anio int(4) NOT NULL,
+  semana varchar(5) COLLATE utf8_spanish_ci NOT NULL,
+  orden_trabajo int(8) NOT NULL,
+  actividad varchar(200),
+  PRIMARY KEY (id_semana)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
