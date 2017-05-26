15bbdf0f07dae914cd8522d6581d5cdbaa4b04be
MauricioHz <mauriciohz2002@gmail.com>
2017-05-25 23:50:47 -0400

    base de datos
    script base de datos

---
+++ script_db.sql
@@ -0,0 +1,3 @@
+INSERT INTO mantencion_plan
+(codigo_plan, fecha_plan, version, anio, nombre_plan, fecha_registro, id_usuario_registro, observacion) 
+VALUES('PLA-G-0.0', '2017-05-25', '1', '2017', 'PLAN DE PRUEBAS', now(), 1, 'Plan para realizar pruebas.');
