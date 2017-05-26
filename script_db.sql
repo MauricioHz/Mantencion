8ba9ee94f92e46b446ae7aaa28b79150b9bdeb8c
MauricioHz <mauriciohz2002@gmail.com>
2017-05-26 13:09:04 -0400

    mantencion de datos

--- script_db.sql
+++ script_db.sql
@@ -1,3 +1,16 @@
+-- Vaciar tablas de parametros
+TRUNCATE TABLE mantencion_plan;
+TRUNCATE TABLE mantencion_area;
+TRUNCATE TABLE mantencion_sub_area;
+
+-- Tablas de resultado
+TRUNCATE TABLE mantencion_equipo_actividad;
+TRUNCATE TABLE mantencion_programa_semana;
+TRUNCATE TABLE mantencion_orden_trabajo;
+
+DELETE TABLE  mantencion_orden_registro;
+
+
 INSERT INTO mantencion_plan
 (codigo_plan, fecha_plan, version, anio, nombre_plan, fecha_registro, id_usuario_registro, observacion) 
 VALUES('PLA-G-0.0', '2017-05-25', '1', '2017', 'PLAN DE PRUEBAS', now(), 1, 'Plan para realizar pruebas.');
