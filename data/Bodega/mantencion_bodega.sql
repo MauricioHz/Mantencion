183dbf3312a322e57eda7d1fe5d9d40d12278092
MauricioHz <mauriciohz2002@gmail.com>
2017-06-05 11:55:45 -0400

    OK

--- data/Bodega/mantencion_bodega.sql
+++ data/Bodega/mantencion_bodega.sql
@@ -1,5 +1,4 @@
 
-
 DROP TABLE IF EXISTS mantencion_bodega;
 CREATE TABLE IF NOT EXISTS mantencion_bodega (
   id_bodega int(2) NOT NULL AUTO_INCREMENT,
@@ -7,13 +6,14 @@ CREATE TABLE IF NOT EXISTS mantencion_bodega (
   descripcion varchar(400) COLLATE utf8_spanish_ci NOT NULL,
   id_tecnico int(3) NOT NULL,
   fecha_registro datetime NOT NULL,
+  vigente boolean,
   PRIMARY KEY (id_bodega)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1;
 
 
-INSERT INTO mantencion_bodega(bodega, descripcion, id_tecnico, fecha_registro) VALUES('BODEGA MAQUINA INNOVA', 'abc1', NOW());
-INSERT INTO mantencion_bodega(bodega, descripcion, id_tecnico, fecha_registro) VALUES('BODEGA MAQUINA SUKAMI', 'abc2', NOW());
-INSERT INTO mantencion_bodega(bodega, descripcion, id_tecnico, fecha_registro) VALUES('BODEGA MAQUINA INNOVA', 'abc3', NOW());
+INSERT INTO mantencion_bodega(bodega, descripcion, id_tecnico, fecha_registro, vigente) VALUES('BODEGA MAQUINA INNOVA', 'abc1', 1, NOW(), TRUE);
+INSERT INTO mantencion_bodega(bodega, descripcion, id_tecnico, fecha_registro, vigente) VALUES('BODEGA MAQUINA SUKAMI', 'abc2', 1, NOW(), FALSE);
+INSERT INTO mantencion_bodega(bodega, descripcion, id_tecnico, fecha_registro, vigente) VALUES('BODEGA MAQUINA INNOVA', 'abc3', 1, NOW(), TRUE);
 
 DROP PROCEDURE IF EXISTS sp_ot_crear_bodega; 
 CREATE PROCEDURE sp_ot_crear_bodega(
@@ -39,14 +39,20 @@ BEGIN
 END
 
 DROP PROCEDURE IF EXISTS sp_ot_actualizar_bodega;
-CREATE PROCEDURE sp_ot_actualizar_bodega(IN bodegaParam varchar(80))
+CREATE PROCEDURE sp_ot_actualizar_bodega(
+    IN idBodegaParam varchar(80)
+    IN bodegaParam varchar(80),
+    IN descripcionParam varchar(400),
+    IN idTecnicoParam int(3)
+)
 BEGIN
     DECLARE cuenta INT(2);
     SET cuenta = (SELECT COUNT(1) FROM mantencion_bodega WHERE bodega = bodegaParam);
     IF cuenta > 0 THEN
         SELECT 'REGISTRO_EXISTE';
     ELSE
-        UPDATE mantencion_bodega SET bodega = bodegaParam, fecha_registro = NOW()        
+        UPDATE mantencion_bodega 
+        SET bodega = bodegaParam, descripcion = descripcionParam, id_tecnico = idTecnicoParam, fecha_registro = NOW()       
         SET @id = LAST_INSERT_ID();
         IF @id > 0 THEN
             SELECT 'ACTUALIZAR_OK';
@@ -58,22 +64,27 @@ END
 
 DROP PROCEDURE IF EXISTS sp_ot_listar_bodega;
 CREATE PROCEDURE sp_ot_listar_bodega()
-    SELECT id_bodega, bodega, fecha_registr FROM mantencion_bodega ORDER BY bodega;
+    SELECT B.id_bodega, B.bodega, B.decripcion, C.id_tecnico, C.tecnico, B.fecha_registro, B.vigente
+    FROM mantencion_bodega AS B INNER JOIN mantencion_tecnico AS C
+    ON B.id_tecnico =  C.id_tecnico
+    ORDER BY bodega;
 
 DROP PROCEDURE IF EXISTS sp_ot_buscar_bodega_id;
 DELIMITER ;;
 CREATE PROCEDURE sp_ot_buscar_bodega_id(IN idBodegaParam int(2))
 BEGIN
-    SELECT id_bodega, bodega, fecha_registro
-    FROM mantencion_bodega
-    WHERE id_bodega = idBodegaParam;
+    SELECT B.id_bodega, B.bodega, B.decripcion, C.id_tecnico, C.tecnico, B.fecha_registro, B.vigente
+    FROM mantencion_bodega AS B INNER JOIN mantencion_tecnico AS C
+    ON B.id_tecnico =  C.id_tecnico
+    WHERE B.id_bodega = idBodegaParam;
+    ORDER BY B.bodega;    
 END ;;
 
 DROP PROCEDURE IF EXISTS sp_ot_eliminar_bodega;
 DELIMITER ;;
 CREATE PROCEDURE sp_ot_eliminar_bodega(IN idBodegaParam varchar(80))
 BEGIN
-        DELETE FROM mantencion_bodega WHERE idBodegaParam = idBodegaParam;       
+        UPDATE mantencion_bodega SET vigente = FALSE WHERE idBodegaParam = idBodegaParam;       
         SET @id = ROW_COUNT();
         IF @id > 0 THEN
             SELECT 'ELIMINAR_OK';
