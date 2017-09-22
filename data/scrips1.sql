ad4ba1d5442b88f4c5715c2c78822d32dffd1eac
MauricioHz <mauriciohz2002@gmail.com>
2017-09-22 09:45:43 -0300

    ok

--- data/scrips1.sql
+++ data/scrips1.sql
@@ -1,1 +1,27 @@
 
+-- mantencion proxima semana y sirve para la actual
+select equipo.equipo_actividad, semana.semana
+from mantencion_programa_semana as semana inner join mantencion_equipo_actividad as equipo
+on equipo.id_equipo = semana.id_equipo
+where semana.semana = 'SEM38'
+order by equipo.equipo_actividad;
+
+-- mantencion pendiente
+select equipo.equipo_actividad, semana.semana
+from mantencion_programa_semana as semana inner join mantencion_equipo_actividad as equipo
+on equipo.id_equipo = semana.id_equipo
+where semana.orden_trabajo is null or semana.orden_trabajo = ''
+order by semana.semana, equipo.equipo_actividad;
+
+-- se encuentran pendientes las siguientes mantenciones:
+-- Existen 34 equipos con mantencion pendiente... ir al listado...
+select count(1) as cantidad_mantenciones_pendientes
+from mantencion_programa_semana as semana inner join mantencion_equipo_actividad as equipo
+on equipo.id_equipo = semana.id_equipo
+where semana.orden_trabajo is null or semana.orden_trabajo = ''
+order by semana.semana, equipo.equipo_actividad;
+
+
+select semana, count()
+from mantencion_programa_semana
+order by semana
