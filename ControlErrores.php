5017de4e6550fcb7b09fdb4116cf0168aabcfc53
MauricioHz <mauriciohz2002@gmail.com>
2018-05-10 21:24:56 -0300

    Update ControlErrores.php

--- ControlErrores.php
+++ ControlErrores.php
@@ -2,20 +2,14 @@
 
 class myClass{
 
-    public function crearRepuestoDisponibleModel(Repuesto_disponible_model $objeto) {
+    public function getData(Repuesto_disponible_model $objeto) {
         $respuesta = new stdClass();
         $sql = 'INSERT INTO mantencion_repuesto_disponible(id_repuesto, id_bodega, repuesto, cantidad, unidad, saldo, cantidad_minimo, precio, vigente)
                 VALUES(:id_repuesto, :id_bodega, :repuesto, :cantidad, :unidad, :saldo, :cantidad_minimo, :precio, "1")';
         try{                
             $statement = $this->db->conn_id->prepare($sql);
             $statement->bindParam(':id_repuesto', $objeto->id_repuesto);
-            $statement->bindParam(':id_bodega', $objeto->id_bodega);
-            $statement->bindParam(':repuesto', $objeto->repuesto);
-            $statement->bindParam(':cantidad', $objeto->cantidad);
-            $statement->bindParam(':unidad', $objeto->unidad);
-            $statement->bindParam(':saldo', $objeto->saldo);
-            $statement->bindParam(':cantidad_minimo', $objeto->cantidad_minimo);
-            $statement->bindParam(':precio', $objeto->precio);
+
             if ($statement->execute()) {
                 $respuesta->mensaje = 'OK';
                 $respuesta->codigo = '1';
